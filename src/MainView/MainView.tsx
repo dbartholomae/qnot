f0ddd7d38dfc0a63b1a9d5b33f2d8428410f7de3
Daniel Bartholomae <daniel@bartholomae.name>
2021-03-28 13:54:43 +0200

    feat: make MainView more beautiful

--- src/MainView/MainView.tsx
+++ src/MainView/MainView.tsx
@@ -15,21 +15,29 @@ export function MainView() {
       <Typography variant="h3" gutterBottom>
         Hi {name ?? "there"}!
       </Typography>
-      <TextFieldForm
-        onConfirmValue={setName}
-        id="name"
-        label={en.MainView.nameLabel}
-        confirmLabel={en.MainView.saveName}
-      />
-      {name && (
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: 12,
+        }}
+      >
         <TextFieldForm
-          onConfirmValue={(roomCode) => push(getRoomPath(roomCode))}
-          id="roomCode"
-          initialValue={randomRoomCode}
-          label={en.MainView.roomCodeLabel}
-          confirmLabel={en.MainView.joinRoom}
+          onConfirmValue={setName}
+          id="name"
+          label={en.MainView.nameLabel}
+          confirmLabel={en.MainView.saveName}
         />
-      )}
+        {name && (
+          <TextFieldForm
+            onConfirmValue={(roomCode) => push(getRoomPath(roomCode))}
+            id="roomCode"
+            initialValue={randomRoomCode}
+            label={en.MainView.roomCodeLabel}
+            confirmLabel={en.MainView.joinRoom}
+          />
+        )}
+      </div>
     </Container>
   );
 }
