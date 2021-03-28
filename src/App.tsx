bdf07cc4b1172bcd0ed04818ce76fe101458eaf1
Daniel Bartholomae <daniel@bartholomae.name>
2021-03-28 13:11:28 +0200

    refactor: extract RoomView

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,14 @@ import { MainView } from "./MainView/MainView";
 import "fontsource-roboto";
 import { Route, Switch } from "react-router-dom";
 
+function RoomView() {
+  return <div>Room</div>;
+}
+
 export function App() {
   return (
     <Switch>
-      <Route path="/r/:roomCode">{() => <div>Room</div>}</Route>
+      <Route path="/r/:roomCode">{() => <RoomView />}</Route>
       <Route path="/">
         <MainView />
       </Route>
