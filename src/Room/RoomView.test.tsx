715d9bd36ce5afa43d08b487a76d5f8d0c8500bc
Daniel Bartholomae <daniel@bartholomae.name>
2021-03-28 17:07:02 +0200

    feat: show my name on room page

--- src/Room/RoomView.test.tsx
+++ src/Room/RoomView.test.tsx
@@ -2,16 +2,19 @@ import { createMemoryHistory, MemoryHistory } from "history";
 import { render, screen } from "@testing-library/react";
 import { Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "../app/store";
+import { createStore } from "../app/store";
 import React from "react";
 import { RoomView } from "./RoomView";
+import { setName } from "../name/nameSlice";
 
 describe("RoomView", () => {
   let history: MemoryHistory;
+  let store: ReturnType<typeof createStore>;
   const roomCode = "test-room-code";
 
   beforeEach(() => {
     history = createMemoryHistory();
+    store = createStore();
     render(
       <Router history={history}>
         <Provider store={store}>
@@ -23,5 +26,11 @@ describe("RoomView", () => {
 
   it("shows the room code", async () => {
     expect(await screen.findByText(roomCode)).toBeInTheDocument();
+  });
+
+  it("shows my name", async () => {
+    const myName = "Daniel";
+    store.dispatch(setName(myName));
+    expect(await screen.findByText(myName)).toBeInTheDocument();
   });
 });
--- src/Room/RoomView.tsx
+++ src/Room/RoomView.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { Container, Typography } from "@material-ui/core";
+import { useSelector } from "../app/store";
+import { selectName } from "../name/nameSlice";
 
 interface Props {
   roomCode: string;
 }
 
 export function RoomView({ roomCode }: Props) {
+  const myName = useSelector(selectName);
   return (
     <Container>
       <Typography variant="h3">Room</Typography>
       <Typography variant="body1">{roomCode}</Typography>
+      <Typography variant="body1">{myName}</Typography>
     </Container>
   );
 }
