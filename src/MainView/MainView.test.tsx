aa99601e7a5e7e35eb2666a0303f96d0ac836836
Daniel Bartholomae <daniel@bartholomae.name>
2021-04-01 20:26:36 +0200

    refactor: move RoomCodeForm tests next to RoomCodeForm

--- src/MainView/MainView.test.tsx
+++ src/MainView/MainView.test.tsx
@@ -4,7 +4,6 @@ import userEvent from "@testing-library/user-event";
 import { createMemoryHistory, MemoryHistory } from "history";
 import { MainView } from "./MainView";
 import { en } from "../locale";
-import { getRoomPath } from "../RoomView/getRoomPath";
 import { createTestProviders } from "../testUtils/createTestProviders";
 
 const locale = en.MainView;
@@ -12,95 +11,31 @@ const locale = en.MainView;
 describe("MainView", () => {
   let history: MemoryHistory;
   const name = "Daniel";
-  const roomCode = "a-room-code";
 
-  describe("on the root path", () => {
-    beforeEach(() => {
-      history = createMemoryHistory({ initialEntries: ["/"] });
-      render(<MainView />, { wrapper: createTestProviders({ history }) });
-    });
-
-    it("greets you if no name is set", () => {
-      expect(screen.getByText("Hi there!")).toBeInTheDocument();
-    });
-
-    it("greets you with your name if you set it", async () => {
-      userEvent.type(screen.getByLabelText(locale.nameLabel), name);
-      userEvent.click(screen.getByText(en.MainView.saveName));
-
-      expect(await screen.findByText("Hi Daniel!")).toBeInTheDocument();
-    });
-
-    it("shows the choose room view after you set the name", async () => {
-      userEvent.type(screen.getByLabelText(locale.nameLabel), name);
-      userEvent.click(screen.getByText(en.MainView.saveName));
-
-      expect(await screen.findByText(en.MainView.joinRoom)).toBeInTheDocument();
-    });
-
-    it("shows a random three-word room code on start", async () => {
-      userEvent.type(screen.getByLabelText(locale.nameLabel), name);
-      userEvent.click(screen.getByText(en.MainView.saveName));
-
-      expect(
-        ((await screen.findByLabelText(
-          en.MainView.roomCodeLabel
-        )) as HTMLInputElement).value
-      ).toMatch(/\w+-\w+-\w+/);
-    });
-
-    it("does not show the choose room view", () => {
-      expect(screen.queryByText(en.MainView.joinRoom)).not.toBeInTheDocument();
-    });
-
-    it("creates a new room code when pressing the reload room code button", async () => {
-      userEvent.type(screen.getByLabelText(locale.nameLabel), name);
-      userEvent.click(screen.getByText(en.MainView.saveName));
-
-      const oldRoomCode = ((await screen.findByLabelText(
-        en.MainView.roomCodeLabel
-      )) as HTMLInputElement).value;
-
-      userEvent.click(screen.getByLabelText(en.MainView.createNewRoomCode));
-
-      expect(
-        ((await screen.findByLabelText(
-          en.MainView.roomCodeLabel
-        )) as HTMLInputElement).value
-      ).not.toBe(oldRoomCode);
-    });
-
-    it("redirects to the room page when joining a room", async () => {
-      userEvent.type(screen.getByLabelText(locale.nameLabel), name);
-      userEvent.click(screen.getByText(en.MainView.saveName));
-
-      userEvent.type(
-        screen.getByLabelText(en.MainView.roomCodeLabel),
-        roomCode
-      );
-      userEvent.click(await screen.findByText(en.MainView.joinRoom));
-
-      expect(history.location.pathname).toEqual(getRoomPath(roomCode));
-    });
+  beforeEach(() => {
+    history = createMemoryHistory({ initialEntries: ["/"] });
+    render(<MainView />, { wrapper: createTestProviders({ history }) });
   });
 
-  describe("on the root path with a room code in the search", () => {
-    beforeEach(() => {
-      history = createMemoryHistory({
-        initialEntries: [`/?roomCode=${roomCode}`],
-      });
-      render(<MainView />, { wrapper: createTestProviders({ history }) });
-    });
+  it("greets you if no name is set", () => {
+    expect(screen.getByText("Hi there!")).toBeInTheDocument();
+  });
 
-    it("loads the initial room name from the search", async () => {
-      userEvent.type(screen.getByLabelText(locale.nameLabel), name);
-      userEvent.click(screen.getByText(en.MainView.saveName));
+  it("greets you with your name if you set it", async () => {
+    userEvent.type(screen.getByLabelText(locale.nameLabel), name);
+    userEvent.click(screen.getByText(en.MainView.saveName));
 
-      expect(
-        ((await screen.findByLabelText(
-          en.MainView.roomCodeLabel
-        )) as HTMLInputElement).value
-      ).toBe(roomCode);
-    });
+    expect(await screen.findByText("Hi Daniel!")).toBeInTheDocument();
+  });
+
+  it("shows the choose room view after you set the name", async () => {
+    userEvent.type(screen.getByLabelText(locale.nameLabel), name);
+    userEvent.click(screen.getByText(en.MainView.saveName));
+
+    expect(await screen.findByText(en.MainView.joinRoom)).toBeInTheDocument();
+  });
+
+  it("does not show the choose room view", () => {
+    expect(screen.queryByText(en.MainView.joinRoom)).not.toBeInTheDocument();
   });
 });
---
+++ src/MainView/RoomCodeForm.test.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { createMemoryHistory, MemoryHistory } from "history";
+import { en } from "../locale";
+import { getRoomPath } from "../RoomView/getRoomPath";
+import { createTestProviders } from "../testUtils/createTestProviders";
+import { RoomCodeForm } from "./RoomCodeForm";
+
+const locale = en.MainView;
+
+describe("RoomCodeForm", () => {
+  let history: MemoryHistory;
+  const roomCode = "a-room-code";
+
+  describe("on the root path", () => {
+    beforeEach(() => {
+      history = createMemoryHistory({ initialEntries: ["/"] });
+      render(<RoomCodeForm />, { wrapper: createTestProviders({ history }) });
+    });
+
+    it("shows a random three-word room code on start", async () => {
+      expect(
+        ((await screen.findByLabelText(
+          en.MainView.roomCodeLabel
+        )) as HTMLInputElement).value
+      ).toMatch(/\w+-\w+-\w+/);
+    });
+
+    it("creates a new room code when pressing the reload room code button", async () => {
+      const oldRoomCode = ((await screen.findByLabelText(
+        en.MainView.roomCodeLabel
+      )) as HTMLInputElement).value;
+
+      userEvent.click(screen.getByLabelText(en.MainView.createNewRoomCode));
+
+      expect(
+        ((await screen.findByLabelText(
+          en.MainView.roomCodeLabel
+        )) as HTMLInputElement).value
+      ).not.toBe(oldRoomCode);
+    });
+
+    it("redirects to the room page when joining a room", async () => {
+      userEvent.type(
+        screen.getByLabelText(en.MainView.roomCodeLabel),
+        roomCode
+      );
+      userEvent.click(await screen.findByText(en.MainView.joinRoom));
+
+      expect(history.location.pathname).toEqual(getRoomPath(roomCode));
+    });
+  });
+
+  describe("on the root path with a room code in the search", () => {
+    beforeEach(() => {
+      history = createMemoryHistory({
+        initialEntries: [`/?roomCode=${roomCode}`],
+      });
+      render(<RoomCodeForm />, { wrapper: createTestProviders({ history }) });
+    });
+
+    it("loads the initial room name from the search", async () => {
+      expect(
+        ((await screen.findByLabelText(
+          en.MainView.roomCodeLabel
+        )) as HTMLInputElement).value
+      ).toBe(roomCode);
+    });
+  });
+});
