b238b4ff274fba220f47b69fd926c6f9aecc0383
Daniel Bartholomae <daniel@bartholomae.name>
2021-04-06 20:23:38 +0200

    fix: avoid unnecessary loading of name from local state
    It is loaded on store setup

--- src/name/useName.ts
+++ src/name/useName.ts
@@ -1,8 +1,7 @@
 import { useDispatch } from "../store/useDispatch";
 import { selectName, setName as setNameAction } from "./nameSlice";
 import { useSelector } from "../store/useSelector";
-import { useEffect } from "react";
-import { getFromLocalStorage, saveToLocalStorage } from "../localStorage";
+import { saveToLocalStorage } from "../localStorage";
 
 export function useName() {
   const name = useSelector(selectName);
@@ -11,11 +10,5 @@ export function useName() {
     saveToLocalStorage("name", newName);
     dispatch(setNameAction(newName));
   };
-  useEffect(() => {
-    const nameFromLocalStorage = getFromLocalStorage("name");
-    if (nameFromLocalStorage !== null) {
-      setName(nameFromLocalStorage);
-    }
-  });
   return [name, setName] as const;
 }
