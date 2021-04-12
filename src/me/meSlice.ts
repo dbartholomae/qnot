4243b9f905434f8528a3262ed25804274583847f
Daniel Bartholomae <daniel@bartholomae.name>
2021-04-12 21:36:51 +0200

    refactor: move name into substate of me slice

--- src/me/meSlice.ts
+++ src/me/meSlice.ts
@@ -1,20 +1,28 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
 import { RootState } from "../store/store";
 import { getFromLocalStorage } from "../localStorage";
 
-type MeState = string | null;
+type MeState = {
+  name: string | null;
+};
 
-const initialState: MeState = getFromLocalStorage("name");
+const initialState: MeState = {
+  name: getFromLocalStorage("name"),
+};
 
 export const meSlice = createSlice({
   name: "me",
   initialState,
   reducers: {
-    setName: (state, action: PayloadAction<string>) => action.payload,
+    setName: (state, action: PayloadAction<string>) => ({
+      ...state,
+      name: action.payload,
+    }),
   },
 });
 
-export const selectName = (state: RootState) => state.me;
+export const selectName = (state: RootState) => state.me.name;
 
 export const { setName } = meSlice.actions;
 export const reducer = meSlice.reducer;
