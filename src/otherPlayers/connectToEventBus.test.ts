95b9a5d2f0a71c0905c0d389c134f2794104c9d6
Daniel Bartholomae <daniel@bartholomae.name>
2021-04-12 22:10:49 +0200

    feat: ignore own joinRoom events

--- src/otherPlayers/connectToEventBus.test.ts
+++ src/otherPlayers/connectToEventBus.test.ts
@@ -2,6 +2,7 @@ import { MockEventBus } from "../eventBus/MockEventBus";
 import { createStore } from "../store/store";
 import { selectPlayers } from "./otherPlayersSlice";
 import { connectToEventBus } from "./connectToEventBus";
+import { selectId } from "../me/meSlice";
 
 describe("connectToEventBus", () => {
   it("adds a player who joins to the players list", () => {
@@ -12,6 +13,18 @@ describe("connectToEventBus", () => {
     const id = "550e8400-e29b-11d4-a716-446655440000";
     eventBus.publish("joinRoom", { id, name });
     expect(selectPlayers(store.getState())).toContainEqual(
+      expect.objectContaining({ id, name })
+    );
+  });
+
+  it("ignores my own join events", () => {
+    const eventBus = new MockEventBus();
+    const store = createStore();
+    connectToEventBus(eventBus, store);
+    const name = "Daniel";
+    const id = selectId(store.getState());
+    eventBus.publish("joinRoom", { id, name });
+    expect(selectPlayers(store.getState())).not.toContainEqual(
       expect.objectContaining({ id, name })
     );
   });
--- src/otherPlayers/connectToEventBus.ts
+++ src/otherPlayers/connectToEventBus.ts
@@ -2,9 +2,14 @@ import { EventBus } from "../eventBus/EventBus";
 import { Store } from "../store/store";
 import { Player } from "./Player";
 import { addPlayer } from "./otherPlayersSlice";
+import { selectId } from "../me/meSlice";
 
 export function connectToEventBus(eventBus: EventBus, store: Store) {
   eventBus.subscribe("joinRoom", (message) => {
+    const myId = selectId(store.getState());
+    if (message.data.id === myId) {
+      return;
+    }
     store.dispatch(
       addPlayer(
         new Player({
