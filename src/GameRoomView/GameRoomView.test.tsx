4a90a02012c3df09f4009510d72316fe88ab67f1
Daniel Bartholomae <daniel@bartholomae.name>
2021-04-27 22:20:30 +0200

    refactor: restructure test

--- src/GameRoomView/GameRoomView.test.tsx
+++ src/GameRoomView/GameRoomView.test.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createTestProviders } from "../testUtils/createTestProviders";
 import { render, screen } from "@testing-library/react";
-import { createStore } from "../store/store";
+import { createStore, Store } from "../store/store";
 import { selectMyWord, startGame } from "../game/gameSlice";
 import { en } from "../locale";
 import { GameRoomView } from "./GameRoomView";
@@ -10,53 +10,64 @@ import { selectId } from "../me/meSlice";
 import { MockPlayer } from "../game/MockPlayer";
 
 describe("GameRoomView", () => {
-  it("shows my word", async () => {
-    const store = createStore();
-    store.dispatch(
-      startGame({
-        players: [
-          new Player({
-            id: selectId(store.getState()),
-            name: "Daniel",
-            isOnline: true,
-          }),
-          new MockPlayer(),
-          new MockPlayer(),
-        ],
-        seed: "seed2",
-        wordList: ["foo", "bar", "baz"],
-      })
-    );
-    render(<GameRoomView />, { wrapper: createTestProviders({ store }) });
-    const myWord = selectMyWord(store.getState());
-    expect(myWord).not.toBeNull();
-    expect(
-      await screen.findByLabelText(en.GameRoomView.myWordLabel)
-    ).toContainHTML(myWord!);
+  let store: Store;
+
+  beforeEach(() => {
+    store = createStore();
   });
 
-  it("shows I'm a question mark if I am", async () => {
-    const store = createStore();
-    store.dispatch(
-      startGame({
-        players: [
-          new Player({
-            id: selectId(store.getState()),
-            name: "Daniel",
-            isOnline: true,
-          }),
-          new MockPlayer(),
-          new MockPlayer(),
-        ],
-        seed: "seed",
-        wordList: ["foo", "bar", "baz"],
-      })
-    );
-    render(<GameRoomView />, { wrapper: createTestProviders({ store }) });
-    const myWord = selectMyWord(store.getState());
-    expect(myWord).toBeNull();
-    expect(
-      await screen.findByText(en.GameRoomView.youReTheQuestionMark)
-    ).toBeInTheDocument();
+  describe("with me having a word", () => {
+    beforeEach(() => {
+      store.dispatch(
+        startGame({
+          players: [
+            new Player({
+              id: selectId(store.getState()),
+              name: "Daniel",
+              isOnline: true,
+            }),
+            new MockPlayer(),
+            new MockPlayer(),
+          ],
+          seed: "seed2",
+          wordList: ["foo", "bar", "baz"],
+        })
+      );
+    });
+
+    it("shows my word", async () => {
+      render(<GameRoomView />, { wrapper: createTestProviders({ store }) });
+      const myWord = selectMyWord(store.getState());
+      expect(
+        await screen.findByLabelText(en.GameRoomView.myWordLabel)
+      ).toContainHTML(myWord!);
+    });
+  });
+
+  describe("with me being a question mark", () => {
+    beforeEach(() => {
+      store.dispatch(
+        startGame({
+          players: [
+            new Player({
+              id: selectId(store.getState()),
+              name: "Daniel",
+              isOnline: true,
+            }),
+            new MockPlayer(),
+            new MockPlayer(),
+          ],
+          seed: "seed",
+          wordList: ["foo", "bar", "baz"],
+        })
+      );
+    });
+
+    it("shows I'm a question mark", async () => {
+      render(<GameRoomView />, { wrapper: createTestProviders({ store }) });
+      expect(
+        await screen.findByText(en.GameRoomView.youReTheQuestionMark)
+      ).toBeInTheDocument();
+    });
   });
 });
