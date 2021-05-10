4cf88617e0188661f58ba8444b5a9182cf616c1d
Daniel Bartholomae <daniel@bartholomae.name>
2021-05-10 20:55:30 +0200

    refactor: extract DescriptionForm

--- src/views/GameRoomView/AddDescriptionView.tsx
+++ src/views/GameRoomView/AddDescriptionView.tsx
@@ -1,50 +1,14 @@
-import React, { useState } from "react";
-import { Button, Container, TextField, Typography } from "@material-ui/core";
-import { en } from "../../services/locale";
+import React from "react";
+import { Container, Typography } from "@material-ui/core";
 import {
   useAddFirstDescription,
   useAddSecondDescription,
 } from "../../business-logic/game";
 import { MyWord } from "./MyWord";
+import { DescriptionForm } from "./DescriptionForm";
 
 export interface Props {
   onChoose: (description: string) => void;
-}
-
-function DescriptionForm({ onChoose }: Props) {
-  const [description, setDescription] = useState("");
-  const [descriptionChosen, setDescriptionChosen] = useState(false);
-  return (
-    <form
-      noValidate
-      onSubmit={(event) => {
-        event.preventDefault();
-        onChoose(description);
-        setDescriptionChosen(true);
-      }}
-      style={{
-        display: "flex",
-        gap: 12,
-      }}
-    >
-      <TextField
-        disabled={descriptionChosen}
-        id="description"
-        label={en.GameRoomView.describeYourWord}
-        value={description}
-        variant="filled"
-        onChange={(event) => setDescription(event.target.value)}
-      />
-      <Button
-        disabled={descriptionChosen}
-        type="submit"
-        variant="contained"
-        color="primary"
-      >
-        {descriptionChosen ? "Waiting for other players" : "Choose this word"}
-      </Button>
-    </form>
-  );
 }
 
 export function AddDescriptionView({ onChoose }: Props) {
---
+++ src/views/GameRoomView/DescriptionForm.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { Button, TextField } from "@material-ui/core";
+import { en } from "../../services/locale";
+
+export interface Props {
+  onChoose: (description: string) => void;
+}
+
+export function DescriptionForm({ onChoose }: Props) {
+  const [description, setDescription] = useState("");
+  const [descriptionChosen, setDescriptionChosen] = useState(false);
+  return (
+    <form
+      noValidate
+      onSubmit={(event) => {
+        event.preventDefault();
+        onChoose(description);
+        setDescriptionChosen(true);
+      }}
+      style={{
+        display: "flex",
+        gap: 12,
+      }}
+    >
+      <TextField
+        disabled={descriptionChosen}
+        id="description"
+        label={en.GameRoomView.describeYourWord}
+        value={description}
+        variant="filled"
+        onChange={(event) => setDescription(event.target.value)}
+      />
+      <Button
+        disabled={descriptionChosen}
+        type="submit"
+        variant="contained"
+        color="primary"
+      >
+        {descriptionChosen ? "Waiting for other players" : "Choose this word"}
+      </Button>
+    </form>
+  );
+}
