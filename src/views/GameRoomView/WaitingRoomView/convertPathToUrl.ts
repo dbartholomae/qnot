08d56cc8bf6aa18bc5244bb4ae9f965be92dc2a9
Daniel Bartholomae <daniel@bartholomae.name>
2021-09-04 22:02:27 +0200

    fix: allow convertPathToUrl to run without env vars

--- src/views/GameRoomView/WaitingRoomView/convertPathToUrl.ts
+++ src/views/GameRoomView/WaitingRoomView/convertPathToUrl.ts
@@ -1,3 +1,5 @@
 export function convertPathToUrl(path: string) {
-  return `${window.location.protocol}//${window.location.host}${process.env.PUBLIC_URL}${path}`;
+  return `${window.location.protocol}//${window.location.host}${
+    process.env.PUBLIC_URL ?? ""
+  }${path}`;
 }
