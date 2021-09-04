146212dc571c80506002ece7882f3af2757bfc41
Daniel Bartholomae <daniel@bartholomae.name>
2021-09-04 23:23:14 +0200

    refactor: create store before tests

--- src/business-logic/me/meSlice.test.ts
+++ src/business-logic/me/meSlice.test.ts
@@ -1,25 +1,26 @@
-import { createStore } from "../store";
+import { MockChannel } from "../../services/channel/MockChannel";
+import { createStore, Store } from "../store";
 import { selectId, selectName, setName } from "./meSlice";
 
 describe("meSlice", () => {
+  let store: Store;
+
   beforeEach(() => {
     localStorage.clear();
+    store = createStore(() => new MockChannel());
   });
 
   it("starts with no name", () => {
-    const store = createStore();
     expect(selectName(store.getState())).toBeNull();
   });
 
   it("allows to update the name", () => {
-    const store = createStore();
     const name = "Daniel";
     store.dispatch(setName(name));
     expect(selectName(store.getState())).toBe(name);
   });
 
   it("starts with an id", () => {
-    const store = createStore();
     expect(typeof selectId(store.getState())).toBe("string");
   });
 });
